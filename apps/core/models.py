bca0abd2ee8901148a15c0aa2b14297a8b4c1b7a
Josh Thompson <jpthompson93@gmail.com>
2019-12-08 23:18:24 -0800

    debugged models.py and added __str__

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class About(models.Model): 
     title = models.CharField(max_length=300)
@@ -7,8 +8,14 @@ class About(models.Model):
     sub_title_2 = models.CharField(max_length=300)
     content_2 = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 class Project(models.Model): 
     title = models.CharField(max_length=300)
     sub_title = models.CharField(max_length=300)
     link = models.CharField(max_length=300)
     image = models.ImageField(upload_to='apps/core/static/images/')
+
+    def __str__(self):
+        return self.title
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from .models import About, Project
 
 def homepage(request):
